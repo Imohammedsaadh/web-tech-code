2ca86c8875ea061d83da8c18c749a28a7af78a13
Proxy <imohammedsaadh@gmail.com>
2025-10-29 08:10:59 +0530

    inriduction of javaScript

---
+++ css/intro-js/d-1 note.md
@@ -0,0 +1,18 @@
+<!-- how to declare variable  -->
+for declaring variable we have 3keyword.
+1.Let
+2.var
+3const
+
+<!-- let vs var -->
+  # In Let-Keyword : Redlarion is not possible.
+ # n var-Keyword : Redlarion is  possible.
+<!-- const -->
+# its used to constant value 
+# here  we can not do re-declaration is not possible 
+# here declaration and initialization we have to do in same line 
+<!-- data-type  -->
+In javascript we have 2 types :
+# primitive
+# non primitive 
+<!-- primitive datatype  -->
---
+++ css/intro-js/intro.js
@@ -0,0 +1,52 @@
+console.log("i am external")
+let personname="santhos";
+console.log(personname);
+
+
+// var-keyword :
+var age=21;// declartion
+var age=41;// Re-declartion
+console.log(age);
+
+
+//const-keyword:
+const aadharNo = 9884867861;
+console.log(aadharNo);// we cant declare again 
+
+// 1 premitive :
+let phno= 234567890;
+console.log(phno);
+
+
+//boolean:
+let hasGf = true ;
+let haschild =false 
+console.log(typeof hasGf);
+console.log(typeof haschild);
+
+
+// string:
+let favactor = "vijay "
+let favactress = 'samantha'
+let secondactor =`dewakar`
+console.log(favactor);
+console.log(favactress);
+console.log(secondactor);
+
+ 
+// undefined
+let city // any variable thats declare but not initialized.
+console.log(city);
+console.log(typeof city); 
+
+
+
+
+
+
+
+
+
+
+
+
